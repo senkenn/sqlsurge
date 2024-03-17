1107f083b3c9c54940208efdc4c36bdef9dafdc5
senkenn <senken32@gmail.com>
2024-03-18 02:05:05 +0900

    Add vscode.window.showInformationMessage() to extension.ts

--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -1,3 +1,6 @@
+import * as vscode from 'vscode'
+
 export function activate(context: any) {
   console.log('Congratulations, your extension "vscode-extension" is now active!')
+  vscode.window.showInformationMessage('Hello World !')
 }
