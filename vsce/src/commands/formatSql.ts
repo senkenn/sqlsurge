ca8ba5b99bcb59e7b411b6fa73e904277e99775c
senkenn <senken32@gmail.com>
2024-05-25 16:26:19 +0900

    fix: fix SQL formatting in formatSql.ts

--- vsce/src/commands/formatSql.ts
+++ vsce/src/commands/formatSql.ts
@@ -20,65 +20,64 @@ export async function commandFormatSqlProvider(
       const sqlNodes = await refresh(document);
       const dummyPlaceHolder = '"SQLSURGE_DUMMY"';
 
-      const edit = new vscode.WorkspaceEdit();
-      sqlNodes.map((sqlNode) => {
-        // get prefix and suffix of space or new line
-        const prefixMatch = sqlNode.content.match(/^(\s*)/);
-        const prefix = prefixMatch ? prefixMatch[0] : "";
-        const suffixMatch = sqlNode.content.match(/(\s*)$/);
-        const suffix = suffixMatch ? suffixMatch[0] : "";
+      editor.edit((editBuilder) => {
+        sqlNodes.map((sqlNode) => {
+          // get prefix and suffix of space or new line
+          const prefixMatch = sqlNode.content.match(/^(\s*)/);
+          const prefix = prefixMatch ? prefixMatch[0] : "";
+          const suffixMatch = sqlNode.content.match(/(\s*)$/);
+          const suffix = suffixMatch ? suffixMatch[0] : "";
 
-        // convert place holder to dummy if there are any place holders
-        const placeHolderRegExp =
-          document.languageId === "typescript"
-            ? /\$\{.*\}/g
-            : document.languageId === "rust"
-              ? /(\$\d+|\?)/g
-              : undefined;
-        if (placeHolderRegExp === undefined) {
-          throw new Error("placeHolderRegExp is undefined");
-        }
+          // convert place holder to dummy if there are any place holders
+          const placeHolderRegExp =
+            document.languageId === "typescript"
+              ? /\$\{.*\}/g
+              : document.languageId === "rust"
+                ? /(\$\d+|\?)/g
+                : undefined;
+          if (placeHolderRegExp === undefined) {
+            throw new Error("placeHolderRegExp is undefined");
+          }
 
-        const placeHolders = sqlNode.content.match(placeHolderRegExp);
-        if (placeHolders) {
-          sqlNode.content = sqlNode.content.replaceAll(
-            placeHolderRegExp,
-            dummyPlaceHolder,
-          );
-        }
-
-        // get formatted content
-        const formattedContentWithDummy = format(sqlNode.content);
-
-        // reverse the place holders
-        let formattedContent = formattedContentWithDummy;
-        if (placeHolders) {
-          placeHolders.forEach((placeHolder, index) => {
-            formattedContent = formattedContent.replace(
+          const placeHolders = sqlNode.content.match(placeHolderRegExp);
+          if (placeHolders) {
+            sqlNode.content = sqlNode.content.replaceAll(
+              placeHolderRegExp,
               dummyPlaceHolder,
-              placeHolder,
             );
-          });
-        }
+          }
 
-        // replace with formatted content
-        edit.replace(
-          document.uri,
-          new vscode.Range(
-            new vscode.Position(
-              sqlNode.code_range.start.line,
-              sqlNode.code_range.start.character,
+          // get formatted content
+          const formattedContentWithDummy = format(sqlNode.content);
+
+          // reverse the place holders
+          let formattedContent = formattedContentWithDummy;
+          if (placeHolders) {
+            placeHolders.forEach((placeHolder, index) => {
+              formattedContent = formattedContent.replace(
+                dummyPlaceHolder,
+                placeHolder,
+              );
+            });
+          }
+
+          // replace with formatted content
+          editBuilder.replace(
+            new vscode.Range(
+              new vscode.Position(
+                sqlNode.code_range.start.line,
+                sqlNode.code_range.start.character,
+              ),
+              new vscode.Position(
+                sqlNode.code_range.end.line,
+                sqlNode.code_range.end.character,
+              ),
             ),
-            new vscode.Position(
-              sqlNode.code_range.end.line,
-              sqlNode.code_range.end.character,
-            ),
-          ),
-          prefix + formattedContent + suffix,
-        );
+            prefix + formattedContent + suffix,
+          );
+        });
       });
 
-      vscode.workspace.applyEdit(edit);
       logger.info("[commandFormatSqlProvider]", "Formatted");
     } catch (error) {
       logger.error(`[commandFormatSqlProvider] ${error}`);
