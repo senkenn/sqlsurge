4de392aee7f45bc9691e17c56b9c201f3e012178
senkenn <senken32@gmail.com>
2024-05-28 09:10:33 +0900

    refactor: Update package.json with new SQL formatting options
    This commit adds the "sqlsurge.formatSql.indent" boolean property to the package.json file, allowing users to choose whether they want SQL code to be formatted with indentation. By default, SQL code will not be indented. This change provides more flexibility for users to customize their SQL formatting preferences.

--- vsce/src/commands/formatSql.ts
+++ vsce/src/commands/formatSql.ts
@@ -3,85 +3,140 @@ import type { SqlNode } from "@senken/config";
 import { format } from "sql-formatter";
 import * as vscode from "vscode";
 
+// TODO: convert to class for logger
 export async function commandFormatSqlProvider(
   logger: vscode.LogOutputChannel,
-  refresh: (
-    document: vscode.TextDocument,
-  ) => Promise<(SqlNode & { vFileName: string })[]>,
+  refresh: RefreshFunc,
 ) {
-  return vscode.commands.registerCommand("sqlsurge.formatSql", async () => {
-    try {
-      logger.info("[commandFormatSqlProvider]", "Formatting...");
-      const editor = vscode.window.activeTextEditor;
-      if (!editor) {
-        return;
-      }
-      const document = editor.document;
-      const sqlNodes = await refresh(document);
-      const dummyPlaceHolder = '"SQLSURGE_DUMMY"';
+  return vscode.commands.registerCommand("sqlsurge.formatSql", () =>
+    formatSql(logger, refresh),
+  );
+}
 
-      editor.edit((editBuilder) => {
-        sqlNodes.map((sqlNode) => {
-          // get prefix and suffix of space or new line
-          const prefixMatch = sqlNode.content.match(/^(\s*)/);
-          const prefix = prefixMatch ? prefixMatch[0] : "";
-          const suffixMatch = sqlNode.content.match(/(\s*)$/);
-          const suffix = suffixMatch ? suffixMatch[0] : "";
+type RefreshFunc = (
+  document: vscode.TextDocument,
+) => Promise<(SqlNode & { vFileName: string })[]>;
 
-          // convert place holder to dummy if there are any place holders
-          const placeHolderRegExp =
-            document.languageId === "typescript"
-              ? /\$\{.*\}/g
-              : document.languageId === "rust"
-                ? /(\$\d+|\?)/g
-                : undefined;
-          if (placeHolderRegExp === undefined) {
-            throw new Error("placeHolderRegExp is undefined");
-          }
-
-          const placeHolders = sqlNode.content.match(placeHolderRegExp);
-          if (placeHolders) {
-            sqlNode.content = sqlNode.content.replaceAll(
-              placeHolderRegExp,
-              dummyPlaceHolder,
-            );
-          }
-
-          // get formatted content
-          const formattedContentWithDummy = format(sqlNode.content);
-
-          // reverse the place holders
-          let formattedContent = formattedContentWithDummy;
-          if (placeHolders) {
-            placeHolders.forEach((placeHolder, index) => {
-              formattedContent = formattedContent.replace(
-                dummyPlaceHolder,
-                placeHolder,
-              );
-            });
-          }
-
-          // replace with formatted content
-          editBuilder.replace(
-            new vscode.Range(
-              new vscode.Position(
-                sqlNode.code_range.start.line,
-                sqlNode.code_range.start.character,
-              ),
-              new vscode.Position(
-                sqlNode.code_range.end.line,
-                sqlNode.code_range.end.character,
-              ),
-            ),
-            prefix + formattedContent + suffix,
-          );
-        });
-      });
-
-      logger.info("[commandFormatSqlProvider]", "Formatted");
-    } catch (error) {
-      logger.error(`[commandFormatSqlProvider] ${error}`);
-      vscode.window.showErrorMessage(`[commandFormatSqlProvider] ${error}`);
+async function formatSql(
+  logger: vscode.LogOutputChannel,
+  refresh: RefreshFunc,
+): Promise<void> {
+  try {
+    logger.info("[commandFormatSqlProvider]", "Formatting...");
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return;
     }
-  });
+    const document = editor.document;
+    const sqlNodes = await refresh(document);
+    const dummyPlaceHolder = '"SQLSURGE_DUMMY"';
+
+    editor.edit((editBuilder) => {
+      sqlNodes.map((sqlNode) => {
+        // get prefix and suffix of space or new line
+        const prefixMatch = sqlNode.content.match(/^(\s*)/);
+        const prefix = prefixMatch ? prefixMatch[0] : "";
+        const suffixMatch = sqlNode.content.match(/(\s*)$/);
+        const suffix = suffixMatch ? suffixMatch[0] : "";
+
+        // convert place holder to dummy if there are any place holders
+        const placeHolderRegExp =
+          document.languageId === "typescript"
+            ? /\$\{.*\}/g
+            : document.languageId === "rust"
+              ? /(\$\d+|\?)/g
+              : undefined;
+        if (placeHolderRegExp === undefined) {
+          throw new Error("placeHolderRegExp is undefined");
+        }
+
+        const placeHolders = sqlNode.content.match(placeHolderRegExp);
+        if (placeHolders) {
+          sqlNode.content = sqlNode.content.replaceAll(
+            placeHolderRegExp,
+            dummyPlaceHolder,
+          );
+        }
+
+        // get formatted content
+        const formattedContentWithDummy = format(sqlNode.content);
+
+        // reverse the place holders
+        let formattedContent = formattedContentWithDummy;
+        if (placeHolders) {
+          placeHolders.forEach((placeHolder, index) => {
+            formattedContent = formattedContent.replace(
+              dummyPlaceHolder,
+              placeHolder,
+            );
+          });
+        }
+
+        // get vscode settings for indent
+        const formatSqlWithIndent = vscode.workspace
+          .getConfiguration("sqlsurge")
+          .get("formatSql") as {
+          indent: boolean;
+          indentSize: number;
+        };
+
+        if (formatSqlWithIndent.indent) {
+          formattedContent = indentedContent(
+            formattedContent,
+            sqlNode.method_line,
+            formatSqlWithIndent.indentSize,
+          );
+        }
+
+        // replace with formatted content
+        editBuilder.replace(
+          new vscode.Range(
+            new vscode.Position(
+              sqlNode.code_range.start.line,
+              sqlNode.code_range.start.character,
+            ),
+            new vscode.Position(
+              sqlNode.code_range.end.line,
+              sqlNode.code_range.end.character,
+            ),
+          ),
+          prefix + formattedContent + suffix,
+        );
+      });
+    });
+
+    logger.info("[commandFormatSqlProvider]", "Formatted");
+  } catch (error) {
+    logger.error(`[commandFormatSqlProvider] ${error}`);
+    vscode.window.showErrorMessage(`[commandFormatSqlProvider] ${error}`);
+  }
+}
+
+function indentedContent(
+  content: string,
+  methodLine: SqlNode["method_line"],
+  indentSize: number,
+): string {
+  const lineText =
+    vscode.window.activeTextEditor?.document.lineAt(methodLine).text;
+  if (!lineText) {
+    return content;
+  }
+
+  const indents = lineText.match(/^(\s*)/)?.[0];
+  if (!indents) {
+    return content;
+  }
+
+  if (indents.length % indentSize !== 0) {
+    // TODO: log
+    throw new Error("indent size should be multiple of indents length");
+  }
+
+  // FIXME: first line is always plus one indent
+  const oneLevelDown = indents.slice(0, indentSize);
+  return content
+    .split("\n")
+    .map((line) => indents + oneLevelDown + line)
+    .join("\n");
 }
